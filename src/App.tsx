954b00491b1da7cd88dad57829cd561bb33bee25
Allan Juan Silva Araujo <allan_araujo@terceiros.sicredi.com.br>
2023-04-10 10:46:50 -0300

    Show result

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { PagesListing } from "./components/PagesListing";
+import { listPages } from "./utils/listPages";
+
+type Inputs = {
+  firstPage: string;
+  lastPage: string;
+};
 
 function App() {
+  const [inputs, setInputs] = useState<Inputs>({ firstPage: "", lastPage: "" });
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleSubmit() {
+    setSubmitted(true);
+  }
+
+  function handlePageChange(page: keyof Inputs) {
+    return function handleSpecificPageChange(
+      event: React.ChangeEvent<HTMLInputElement>
+    ) {
+      setInputs({ ...inputs, [page]: event.target.value });
+    };
+  }
+
+  function Result(props: { submitted: boolean; inputs: Inputs }) {
+    const { submitted, inputs } = props;
+
+    if (!submitted) {
+      return null;
+    }
+
+    const [firstList, secondList] = listPages(
+      +inputs.firstPage,
+      +inputs.lastPage
+    );
+
+    return (
+      <>
+        <PagesListing pages={firstList}></PagesListing>
+        <PagesListing pages={secondList}></PagesListing>
+      </>
+    );
+  }
+
   return (
     <div>
       <h1>Duda's</h1>
 
-      <form>
+      <div>
         <div>
           <label htmlFor="firstPage">Primeira página</label>
-          <input id="firstPage"></input>
+          <input
+            id="firstPage"
+            value={inputs.firstPage}
+            onChange={handlePageChange("firstPage")}
+          ></input>
         </div>
 
         <div>
           <label htmlFor="lastPage">Última página</label>
-          <input id="lastPage"></input>
+          <input
+            id="lastPage"
+            value={inputs.lastPage}
+            onChange={handlePageChange("lastPage")}
+          ></input>
         </div>
 
-        <button type="submit">Listar páginas</button>
-      </form>
+        <button onClick={handleSubmit}>Listar páginas</button>
+      </div>
+
+      <div>
+        <Result submitted={submitted} inputs={inputs}></Result>
+      </div>
     </div>
   );
 }
--- src/components/PagesListing.tsx
+++ src/components/PagesListing.tsx
@@ -1,5 +1,12 @@
+import { areOdd } from "../utils/are-odd";
 import { formatPageList } from "../utils/formatPageList";
 
-export function PagesListing(pages: number[]) {
-  return <div>{formatPageList(pages)}</div>;
+export function PagesListing(props: { pages: number[] }) {
+  const label = areOdd(props.pages) ? "Ímpares" : "Pares";
+  return (
+    <div>
+      <h2>{label}</h2>
+      <span>{formatPageList(props.pages)}</span>
+    </div>
+  );
 }
