f8a281fde896f59e818b098d32ac4ae7590d9b74
Allan Juan Silva Araujo <allan_araujo@terceiros.sicredi.com.br>
2023-04-10 09:54:39 -0300

    Create basic page structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,25 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Duda's</h1>
+
+      <form>
+        <div>
+          <label htmlFor="firstPage">Primeira página</label>
+          <input id="firstPage"></input>
+        </div>
+
+        <div>
+          <label htmlFor="lastPage">Última página</label>
+          <input id="lastPage"></input>
+        </div>
+
+        <button type="submit">Listar páginas</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
