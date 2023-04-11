ed9bf118c8452c94e71e12a373c33e099b8002f1
Allan Juan Silva Araujo <allan_araujo@terceiros.sicredi.com.br>
2023-04-11 11:05:59 -0300

    Copy list of pages

--- src/components/PagesListing.tsx
+++ src/components/PagesListing.tsx
@@ -1,19 +1,25 @@
 import "../App.css";
 import { areOdd } from "../utils/areOdd";
+import { copyToClipboard } from "../utils/copyToClipboard";
 import { formatPageList } from "../utils/formatPageList";
 
 export function PagesListing(props: { pages: number[] }) {
   const label = areOdd(props.pages) ? "Ímpares" : "Pares";
   const labelAndQuantity = `${label} (${props.pages.length})`;
+  const formattedPageList = formatPageList(props.pages);
+
+  function copyPageList() {
+    copyToClipboard(formattedPageList);
+  }
 
   return (
     <div className="pages-listing-container">
       <div>
         <h2 className="listing-label">{labelAndQuantity}</h2>
-        <span className="page-list">{formatPageList(props.pages)}</span>
+        <span className="page-list">{formattedPageList}</span>
       </div>
 
-      <button className="round-button"></button>
+      <button className="round-button" onClick={copyPageList}></button>
     </div>
   );
 }
